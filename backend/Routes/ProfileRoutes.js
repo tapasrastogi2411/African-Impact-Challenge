b0d3508b822ea164533a39cf2ec062d644622799
Brandon Jaipersaud <brandon.jaipersaud@mail.utoronto.ca>
2021-06-09 20:20:43 -0400

    [TT-2] Add appropriate response codes

--- backend/Routes/ProfileRoutes.js
+++ backend/Routes/ProfileRoutes.js
@@ -4,16 +4,73 @@ var auth = require('../Middleware/auth');
 const db = require('../db');
 
 
-router.post('/register/:username', function (req, res, next) {
-    var query = "INSERT INTO profile_schema.\"Company\"(company_id,name,address) VALUES (60,'B','F')";
-    // uses JS promises
-    db.query(query, [])
-		.then(pgRes => {
-            res.status(200).json('');
+router.post('/register/', function (req, res, next) {
+    var registerData = req.body;
+    var mandatoryFields = ["username", "password", "user_role","first_name", "last_name"];
+    var optionalFields = ["honorifics", "email", "phone_number", "country", "address"];
+    var schemaOrder = ["username", "password", "user_role","honorifics","first_name", "last_name","email", "phone_number", "country", "address"];
+    // first check for malformed input
+    if (Object.values(registerData).length != 10) {
+        return res.status(400).json({err: 'Should be 10 fields'});
+    }
+
+    var invalidMandatoryFields = []; 
+    var invalidOptionalFields = []; 
+    var userData = [];
+
+    for (let i = 0; i < schemaOrder.length; i++) {
+        var value = schemaOrder[i];
+        // console.log(value);
+        if (mandatoryFields.includes(value)){
+            if (!(value in registerData)){
+                invalidMandatoryFields.push(value);
+            } else {
+                userData.push(registerData[value]);
+            }
+        } 
+        if (optionalFields.includes(value)) {
+            if (!(value in registerData)){
+                invalidOptionalFields.push(value);
+            }
+            else if (registerData[value] == '') {
+                userData.push('null');
+            } else {
+                userData.push(registerData[value]);
+            }
+        }
+    }
+
+    //console.log(invalidMandatoryFields);
+    //console.log(invalidOptionalFields);
+    //console.log(userData);
+
+    if (invalidMandatoryFields.length > 0 ||  invalidOptionalFields.length > 0) {
+        return res.status(400).json({err:"Invalid mandatory or optional fields", mandatory: invalidMandatoryFields,
+        optional: invalidOptionalFields, data: userData});
+    } 
+
+    // all 10 fields are provided and they have the correct names
+
+    var userSchema = "(username,password,user_role,honorifics,first_name,last_name,email,phone_number,country,address)";
+    var preparedValues = "($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)";
+    var query = "INSERT INTO profile_schema.aic_user" + userSchema + " VALUES" + preparedValues;
+    
+    // hash and salt passwords
+    db.query(query, userData)
+		.then(pgRes => { // user successfully added to the db
+            //console.log(pgRes);
+            return res.status(201).json(''); // 201 Created. Any payload to send to the client?
         })
-        .catch(err => {
-            console.log(err.message);
-            res.status(500).json({ error: "Bad Query"});
+        .catch(err => { // user already exists or some other db error
+            switch (err.message){
+                case "duplicate key value violates unique constraint \"aic_user_pkey\"":
+                    return res.status(409).json({ err: 'Username already exists'});
+                
+                default:
+                    console.log(err.message);
+                    return res.status(500).json({ err: "Query error"});
+                    
+            }
         });
 });
 
