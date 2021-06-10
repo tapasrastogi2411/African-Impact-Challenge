a5c1a47d002ce02d8431ec15f2a3f50a0a83bf16
CTprogram <60624555+CTprogram@users.noreply.github.com>
2021-06-10 14:23:09 -0400

    Tt13 user login (#12)
    * signup page update
    
    * signup page update (#9)
    
    Co-authored-by: AllenZhang2000 <alan20000309@gmail.com>
    
    * added temp sign out btn
    
    * putting src files into frontend folder
    
    * putting front end files into frontend folder
    
    * input fields border style update
    
    * Delete frontend directory
    
    Co-authored-by: AllenZhang2000 <alan20000309@gmail.com>
    Co-authored-by: Clement tran <clementtran@Clements-MacBook-Pro-2.local>
    Co-authored-by: patrickkemzang <46250035+patrickkemzang@users.noreply.github.com>
    Co-authored-by: AllenZhang2000 <55424008+AllenZhang2000@users.noreply.github.com>

--- src/Pages/UserProfile/SignUp.tsx
+++ src/Pages/UserProfile/SignUp.tsx
@@ -3,9 +3,13 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { useForm } from "react-hook-form";
 import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
@@ -32,7 +36,7 @@ const useStyles: (props?: any) => any = makeStyles((theme) => ({
     },
     input: {
         background: "white",
-        borderRadius: "20px",
+        //borderRadius: "20px",
         width: 300
     },
     text: {
@@ -42,11 +46,35 @@ const useStyles: (props?: any) => any = makeStyles((theme) => ({
 
 }));
 
-
+const CssTextField = withStyles({
+    root: {
+        '& label.Mui-focused': {
+            color: '#e69113',
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: '#e69113',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                //borderColor: 'black',
+            },
+            '&:hover fieldset': {
+                borderColor: '#fcb040',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: '#e69113',
+            },
+        },
+    },
+})(TextField);
 
 export default function SignUp(props: any) {
     const classes = useStyles();
+    const [role, setRole] = React.useState('');
 
+    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        setRole(event.target.value as string);
+    };
     return (
         <Container component="main" maxWidth="md">
 
@@ -75,7 +103,7 @@ export default function SignUp(props: any) {
                     spacing={3}>
 
                     <Grid item>
-                        <TextField
+                        <CssTextField
                             variant="outlined"
                             required
                             name="Full Name"
@@ -86,18 +114,26 @@ export default function SignUp(props: any) {
                         />
                     </Grid>
                     <Grid item>
-                        <TextField
+                        <FormControl
                             variant="outlined"
-                            required
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
                             className={classes.input}
-                        />
+                            required>
+                            <InputLabel>Role</InputLabel>
+                            <Select
+                                label="Role"
+                                id="role"
+                                value={role}
+                                onChange={handleChange}
+                            >
+                                <MenuItem value="Entrepreneur">Entrepreneur</MenuItem>
+                                <MenuItem value="Instructor">Instructor</MenuItem>
+                                <MenuItem value="Partner">Partner</MenuItem>
+                            </Select>
+                        </FormControl>
+
                     </Grid>
                     <Grid item>
-                        <TextField
+                        <CssTextField
                             variant="outlined"
                             required
                             id="username"
@@ -108,7 +144,7 @@ export default function SignUp(props: any) {
                         />
                     </Grid>
                     <Grid item>
-                        <TextField
+                        <CssTextField
                             variant="outlined"
                             required
                             name="password"
@@ -120,7 +156,7 @@ export default function SignUp(props: any) {
                         />
                     </Grid>
                     <Grid item>
-                        <TextField
+                        <CssTextField
                             variant="outlined"
                             name="phone_number"
                             label="Phone Number"
@@ -130,12 +166,12 @@ export default function SignUp(props: any) {
                         />
                     </Grid>
                     <Grid item>
-                        <TextField
+                        <CssTextField
                             variant="outlined"
-                            name="field"
-                            label="Field"
-                            type="text"
-                            id="field"
+                            id="email"
+                            label="Email"
+                            name="email"
+                            autoComplete="email"
                             className={classes.input}
                         />
                     </Grid>
