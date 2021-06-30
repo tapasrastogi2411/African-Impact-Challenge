5cf709637f8254da1e7b3c58153e0aa954793ab9
Clement tran <clementtran@Clements-MacBook-Pro-2.local>
2021-06-30 16:21:22 -0400

    TT-65 Created Assignment page with Upload Assignment button

---
+++ frontend/src/Pages/AssignmentPage/AssignmentPage.tsx
@@ -0,0 +1,222 @@
+import React, { Component, Fragment } from "react";
+import SignIn from "../UserProfile/LogIn";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Navbar from "../../NavBar/Navbar";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Avatar, Divider, Toolbar } from "@material-ui/core";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "absolute",
+    top: 130,
+    left: 300,
+    width: 1400,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  divider: {
+    width: "150%",
+    height: 3,
+    marginTop: 5,
+    marginBottom: 10,
+  },
+  profilePic: {
+    width: 200,
+    marginTop: 20,
+    borderRadius: 5,
+  },
+  info: {
+    marginTop: 3,
+    marginLeft: 15,
+    maxWidth: 1100,
+  },
+  category: {
+    fontSize: 22,
+    fontWeight: 700,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+  btn: {
+    backgroundColor: "#fcb040",
+    color: "#ffffff",
+    width: 180,
+    "&:hover": { background: "#e69113" },
+    marginLeft: 10,
+    borderRadius: 20,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  input: {
+    display: "none",
+  },
+  pageTitle: {
+    marginLeft: 10,
+  },
+  uploadButton: {
+    marginLeft: 800,
+  },
+}));
+
+function AssignmentPage() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [file, setFile] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleUploadedFile = (e: any) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e: any) => {
+    const formdata = new FormData();
+    formdata.append("assignments", file);
+    formdata.append("type", "assignments");
+    formdata.append("description", description);
+
+    const response = await fetch(
+      "https://localhost:8080/api/assignment/updateAssignment",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        body: formdata,
+        mode: "cors",
+      }
+    );
+    const responseData = await response.json();
+    if (response.status > 300 || response.status < 200) {
+      throw responseData;
+    }
+    handleClose();
+  };
+
+  const assignmentItems = [
+    {
+      text: "first assignment title",
+      path: "frontend/public/logo192.png",
+    },
+    {
+      text: "2nd assignment title",
+      path: "frontend/public/Uploads/Assignments/CSCC43Tutorial1.pdf",
+    },
+  ];
+
+  return (
+    <div>
+      <Navbar></Navbar>
+
+      <Grid container className={classes.root}>
+        <Grid item xs={12} container spacing={2}>
+          <Typography variant="h4" className={classes.pageTitle}>
+            Assignments
+          </Typography>
+          <Grid>
+            <div>
+              <Button
+                variant="outlined"
+                onClick={handleClickOpen}
+                className={classes.uploadButton}
+              >
+                Upload Assignment
+              </Button>
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="form-dialog-title"
+              >
+                <DialogContent>
+                  <DialogContentText>
+                    Please fill in the following fields
+                  </DialogContentText>
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="assignmentTitle"
+                    name="assignmentTitle"
+                    label="Assignment Title"
+                    type="text"
+                    fullWidth
+                  />
+                </DialogContent>
+                <DialogContent>
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="description"
+                    name="description"
+                    label="Description"
+                    type="text"
+                    fullWidth
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </DialogContent>
+
+                <DialogContent>
+                  <TextField
+                    type={"file"}
+                    name="assignments"
+                    id="assignments"
+                    label="assignments"
+                    inputProps={{ accept: "application/pdf" }}
+                    onChange={handleUploadedFile}
+                  ></TextField>
+                  <AssignmentIcon />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleSubmit} color="primary">
+                    Submit
+                  </Button>
+                  <Button onClick={handleClose} color="primary">
+                    Cancel
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </div>
+          </Grid>
+        </Grid>
+
+        <Divider className={classes.divider} />
+        <List component="nav" aria-labelledby="assignmentList">
+          <Typography>Assignment</Typography>
+          {assignmentItems.map((item) => (
+            <ListItem key={item.text} button>
+              <ListItemIcon>
+                <AssignmentIcon />
+              </ListItemIcon>
+              <ListItemText primary={item.text}></ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
+    </div>
+  );
+}
+
+export default AssignmentPage;
