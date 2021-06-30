263c5fa81aaca92d7f70e387f4445ead36c06d0a
Jacob <aaron@Jacobs-MacBook-Pro.local>
2021-06-30 15:14:10 -0400

    TT-69-fix response codes

--- backend/Routes/CoursesRoutes.js
+++ backend/Routes/CoursesRoutes.js
@@ -8,7 +8,7 @@ var upload = require('../Middleware/upload');
 //fields([{name:'readings'},{name: 'videos'}, {name:'assignments'}])
 
 router.post('/upload', auth, upload.any(), function (req, res) { 
-    //req.session.username = "Aaron JACOB"; //uncomment this for testing
+    req.session.username = "Aaron JACOB"; //uncomment this for testing
 
     if(req.files.length === 0) {
         return res.status(400).end();
@@ -42,11 +42,11 @@ router.post('/upload', auth, upload.any(), function (req, res) {
         .query(query, values)
         .then(result => {
             console.log("File successfully uploaded.")
-            return res.status(200);
+            res.status(200).end();
         })
         .catch(e => {
             console.error(e.stack);
-            return res.status(500);
+            res.status(500).end();
         })
 });
 
