79946632b92f86baf1d70a4112a92c1c020687c2
brandonjaipersaud <41272549+brandonjaipersaud@users.noreply.github.com>
2021-07-14 13:56:01 -0400

    Merge TT-23 into Develop
    * Merge TT-90 into TT-23 
    
    * TT-90 update db to store assignment submissions
    
    * TT-90 Add upload file dialog with error checking
    
    * TT-90 Attach upload dialog to submit btn rather than upload assignment btn. Remove upload assign btn from entrepreneur view.
    
    * Update db to store assignment submissions. Create teacher and entrepreneur view.
    
    * TT-90 Update db to store entrepreneur assignment submissions.
    
    * TT-90 Add .gitignore to backend
    
    * TT-90 Entrepreneurs can resubmit assignments:backend
    
    * TT-90 Conditionally add resubmit and view submission buttons:frontend
    
    * TT-90 Fix bug in entrepreneur view
    
    * Merge TT-94 into TT-23
    
    * TT-92 Cleanup form fields
    
    * TT-92 Add deadlines to assignments
    
    * TT-92 Finish deadline functionality
    
    * TT-94 Finish View submission button
    
    * TT-94 Remove console.log
    
    * TT-94 Remove console.log from backend
    
    * TT-23 Fix syntax error in schema

--- backend/Routes/CoursesRoutes.js
+++ backend/Routes/CoursesRoutes.js
@@ -38,14 +38,26 @@ router.get('/getVideos', auth, async (req, res) => {
     }
 });
 
-// Route for getting the `Assignments`
+
+/**
+ * Get assignments and submission info associated with the assignments. Submission info pertains
+ * to the currently logged-in user.
+ * 
+ */
 router.get('/getAssignments', auth, async (req, res) => {
     try{
+        //req.session.username = "Aaron"; //uncomment this for testing
+        let query = "SELECT * " +
+                    "FROM (post_schema.postfile pf join post_schema.postassignment pa on (pf.file_path=pa.file_path)) left outer join (select * " +
+                                                                                                                "from post_schema.submitassignment sa " +
+                                                                                                                " where sa.submission_user = $1) as sa2 on pa.file_path=sa2.assignment_file_path " +
+                    "ORDER BY upload_date";
 
-        let query = `SELECT * FROM post_schema.postfile WHERE category=3 ORDER BY upload_date DESC`;
-        const result = await db.query(query);
+        const result = await db.query(query, [req.session.username]);
+        //console.log(result.rows);
         // const filePaths = result.rows.map(row => row.file_path);
         var fileArray = result.rows;
+        //console.log(fileArray);
         for (obj of fileArray) {
             if (obj['description'] == "") {
                 obj['description'] = "Description not provided";
@@ -55,7 +67,7 @@ router.get('/getAssignments', auth, async (req, res) => {
             }
         }
        
-        console.log(fileArray);
+        //console.log(fileArray);
         return res.status(200).json(fileArray);
     }
 
@@ -65,8 +77,10 @@ router.get('/getAssignments', auth, async (req, res) => {
     }
 });
 
-router.post('/upload', auth, upload.any(), function (req, res) { 
-    // req.session.username = "Aaron JACOB"; //uncomment this for testing
+router.use('/upload', auth, upload.any(), function (req, res, next) { 
+    //req.session.username = "Aaron"; //uncomment this for testing
+    //console.log("In upload route");
+    //console.log(req.files);
 
     if(req.files.length === 0) {
         return res.status(400).end();
@@ -93,21 +107,107 @@ router.post('/upload', auth, upload.any(), function (req, res) {
         title = req.body.title
     } 
 
-    var postfileSchema = "(file_path, category, upload_date, upload_user, description, title)";
-    var preparedValues = "($1,$2,$3,$4,$5, $6)";
+    var postfileSchema = "(file_path, category, upload_date, upload_user, title, description)";
+    var preparedValues = "($1,$2,$3,$4,$5,$6)";
     var query = "INSERT INTO post_schema.postfile" + postfileSchema + " VALUES" + preparedValues;
-    var values = [req.files[0].path.split(path.resolve(__dirname, '../')).pop(), category, datetime, req.session.username, req.body.description, title]   
+    var values = [req.files[0].path.split(path.resolve(__dirname, '../')).pop(), category, datetime, req.session.username, title, req.body.description]   
 
     db
         .query(query, values)
         .then(result => {
-            console.log("File successfully uploaded.")
+            //console.log("File successfully uploaded.");
+            if (fieldName === 'assignments') {
+                next();
+            }
             res.status(200).end();
         })
         .catch(e => {
             console.error(e.stack);
             res.status(500).end();
         })
+});
+
+
+// 2017-05-27T10:30
+router.post('/upload/assignment/teacher', function (req, res) { 
+    console.log("In teacher route");
+
+    let filePath = req.files[0].path.split(path.resolve(__dirname, '../')).pop();
+    let totalMarks = req.body.totalMarks;
+    let deadline = req.body.deadline.replace("T", " ");
+    console.log(deadline);
+    var query = "INSERT INTO post_schema.PostAssignment VALUES ($1, $2, $3)";
+
+    console.log("In teacher route");
+
+    db.query(query, [filePath, totalMarks, deadline])
+    .then(result => {
+        //console.log("Assignment stored in PostAssignment");
+        res.status(200).end();
+    })
+    .catch(e => {
+        console.error(e.stack);
+        res.status(500).end();
+    })
+});
+
+
+
+/**
+ * For simplicity, entrepreneur can make one submission per assignment
+ * Most recent submission is stored
+ * Allows for resubmission
+ */
+router.post('/upload/assignment/entrepreneur',  function (req, res) { 
+    console.log("In entrepreneur route");
+    let submissionPath = req.files[0].path.split(path.resolve(__dirname, '../')).pop();
+    let postedAssignment = JSON.parse(req.body.postedAssignment);
+    let assignmentPath = postedAssignment.file_path;
+    let currentdate = new Date(); 
+    let datetime = currentdate.getFullYear() + "-" 
+            + (currentdate.getMonth()+1) + "-"
+            + currentdate.getDate() + " "
+            + currentdate.getHours() + ":"  
+            + currentdate.getMinutes() + ":" 
+            + currentdate.getSeconds();
+
+    //console.log(submissionPath);
+    //console.log(assignmentPath);
+    //console.log(req.session.username);
+    //console.log("In entrepreneur route");
+
+    let getUserPrevSubmission = "SELECT sa.submission_file_path as file_path FROM post_schema.submitassignment sa where sa.submission_user = $1 AND sa.assignment_file_path = $2  ";
+    let deleteQuerySubmitAssignment = "DELETE FROM post_schema.submitassignment sa WHERE sa.submission_user = $1 AND sa.assignment_file_path = $2";
+    let deleteQueryPostFile = "DELETE FROM post_schema.postfile pf WHERE pf.file_path = $1";
+    let schema = "(submission_file_path, submission_user, assignment_file_path, submission_date)";
+    let insertQuery = "INSERT INTO post_schema.submitassignment" + schema + " values ($1, $2, $3, $4)";
+
+    var prevSubmissionFilePath;
+    db.query(getUserPrevSubmission, [req.session.username, assignmentPath])
+    .then(result => {
+        if (result.rows[0]) {
+            prevSubmissionFilePath = result.rows[0].file_path;
+        } else {
+            prevSubmissionFilePath = "";
+        }
+        //console.log("In P1");
+        //console.log(prevSubmissionFilePath);
+        return db.query(deleteQueryPostFile, [prevSubmissionFilePath])
+    })
+    .then(result => {
+        console.log("Deleted submission stored in postfile");
+        return db.query(insertQuery, [submissionPath, req.session.username, assignmentPath, datetime]);
+    })
+    .then(result => {
+        console.log("Submitted assignment inserted in submitassignment");
+        res.status(200).end();
+    })
+    .catch(e => {
+        console.error(e.stack);
+        res.status(500).end();
+    })
+
+    
 });
 
 module.exports = router;
