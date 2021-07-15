fcdcf585998027d078dd278e0be00717cb610ad2
Jacob <aaron@Jacobs-MacBook-Pro.local>
2021-07-15 19:39:13 -0400

    TT84 allow user to set reading title

--- frontend/src/Pages/ReadingsPage/ReadingsPage.tsx
+++ frontend/src/Pages/ReadingsPage/ReadingsPage.tsx
@@ -13,6 +13,7 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
+import LocalLibraryOutlinedIcon from '@material-ui/icons/LocalLibraryOutlined';
 import DialogTitle from "@material-ui/core/DialogTitle";
 import TextField from "@material-ui/core/TextField";
 import { withStyles } from "@material-ui/core/styles";
@@ -114,6 +115,7 @@ function ReadingPage(prop: any) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [file, setFile] = React.useState("");
+  const [title, setTitle] = React.useState("");
   const [description, setDescription] = React.useState("");
   const [readingItems, setreadingItems] = React.useState([]);
   const [alertMessage, setAlertMessage] = React.useState("");
@@ -135,6 +137,7 @@ function ReadingPage(prop: any) {
   const handleSubmit = async (e: any) => {
     const formData = new FormData();
     formData.append("readings", file);
+    formData.append("title", title);
     formData.append("description", description);
 
     const response = await fetch("http://localhost:8080/api/course/upload", {
@@ -248,59 +251,69 @@ function ReadingPage(prop: any) {
           </Typography>
           <Grid>
             <div>
-              <Button
-                variant="outlined"
-                onClick={handleClickOpen}
-                className={classes.uploadButton}
-              >
-                Upload Readings
-              </Button>
-              <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="form-dialog-title"
-              >
-                <DialogContent>
-                  <DialogContentText>
-                    Please fill in the following fields
-                  </DialogContentText>
-                  <TextField
-                    autoFocus
-                    margin="dense"
-                    id="description"
-                    name="description"
-                    label="description"
-                    type="text"
-                    fullWidth
-                    onChange={(e) => setDescription(e.target.value)}
-                  />
-                </DialogContent>
-
-                <DialogContent>
-                  <TextField
-                    type={"file"}
-                    name="readings"
-                    id="readings"
-                    label="readings"
-                    inputProps={{ accept: "application/pdf,.doc,.docx,.txt" }}
-                    onChange={handleUploadedFile}
-                  ></TextField>
-                  <MenuBookIcon />
-                </DialogContent>
-                <DialogContent>
-                  {alertMessage.length > 0 ? (
-                    <RedTextTypography>{alertMessage}</RedTextTypography>
-                  ) : null}
-                </DialogContent>
-                <DialogActions>
-                  <Button onClick={handleSubmit} color="primary">
-                    Submit
+            <Button
+                      variant="outlined"
+                      onClick={handleClickOpen}
+                      className={classes.uploadButton}
+                      >
+                  Upload Readings
                   </Button>
-                  <Button onClick={handleClose} color="primary">
-                    Cancel
-                  </Button>
-                </DialogActions>
-              </Dialog>
+                  <Dialog
+                      open={open}
+                      onClose={handleClose}
+                      aria-labelledby="form-dialog-title"
+                      >
+                      <DialogContent>
+                        <DialogContentText>
+                            Please fill in the following fields
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="title"
+                            name="title"
+                            label="title"
+                            type="text"
+                            fullWidth
+                            onChange={(e) =>
+                        setTitle(e.target.value)}
+                        />
+                        <TextField
+                            margin="dense"
+                            id="description"
+                            name="description"
+                            label="description"
+                            type="text"
+                            fullWidth
+                            onChange={(e) =>
+                            setDescription(e.target.value)}
+                            />
+                      </DialogContent>
+                      <DialogContent>
+                      <TextField
+                      type={"file"}
+                      name="readings"
+                      id="readings"
+                      label="readings"
+                      inputProps={{ accept: "application/pdf,.doc,.docx,.txt" }}
+                      onChange={handleUploadedFile}
+                      ></TextField>
+                      <LocalLibraryOutlinedIcon />
+                      </DialogContent>
+                      <DialogContent>
+                        {alertMessage.length > 0 ? (
+                        <RedTextTypography>{alertMessage}</RedTextTypography>
+                        ) : null}
+                      </DialogContent>
+                      <DialogActions>
+                        <Button onClick={handleSubmit} color="primary">
+                        Submit
+                        </Button>
+                        <Button onClick={handleClose} color="primary">
+                        Cancel
+                        </Button>
+                      </DialogActions>
+                  </Dialog>
             </div>
           </Grid>
         </Grid>
