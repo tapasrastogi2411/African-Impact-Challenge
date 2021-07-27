4c8cdf8da064ce4be75232c57302b1d1c30943cc
Brandon J <brandon19997@hotmail.com>
2021-07-27 11:57:44 -0400

    TT-123 Test register route on heroku

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,10 +1,12 @@
 const { Pool } = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/aic'
+const connectionStringURI = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/aic'
 
 // pool of clients that can connect to the db
 const pool = new Pool({
-    connectionString,
-    ssl: true
+    connectionString: connectionStringURI,
+    ssl: {
+      rejectUnauthorized: false
+    }
 });
 
 module.exports = {
